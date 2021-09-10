96bdc8613d2fc7764fdc376e9e6cf54a18e1b20d
Jhonatahan Orlando Rivera Vilcapoma <jonathan02rv@gmail.com>
2021-09-10 00:08:28 -0500

    create project

---
+++ MyMoviesApp/MyMoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMoviesApp
+//
+//  Created by Jhonatahan Orlando Rivera Vilcapoma on 10/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
